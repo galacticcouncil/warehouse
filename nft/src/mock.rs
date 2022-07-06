14316087581be36953e2f81f4e92d5d40ce96312
Roznovjak <richardroznovjak@gmail.com>
2022-07-06 14:54:45 +0200

    formatting

--- nft/src/mock.rs
+++ nft/src/mock.rs
@@ -215,5 +215,5 @@ impl ExtBuilder {
 }
 
 pub fn expect_events(e: Vec<Event>) {
-	e.into_iter().for_each(frame_system::Pallet::<Test>::assert_has_event);
+    e.into_iter().for_each(frame_system::Pallet::<Test>::assert_has_event);
 }

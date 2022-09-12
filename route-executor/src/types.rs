e356e9486638f050f492e992238571ae66c03560
dmoka <mokabme@gmail.com>
2022-09-12 08:41:29 +0200

    add doc for trade type

--- route-executor/src/types.rs
+++ route-executor/src/types.rs
@@ -2,6 +2,7 @@ use codec::{Decode, Encode};
 use hydradx_traits::router::PoolType;
 use scale_info::TypeInfo;
 
+///A single trade for buy/sell, describing the asset pair and the pool type in which the trade is executed
 #[derive(Encode, Decode, Debug, Eq, PartialEq, Copy, Clone, TypeInfo)]
 pub struct Trade<AssetId> {
     pub pool: PoolType<AssetId>,

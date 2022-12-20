3b312150419d1848a11b884a394a9f3832a3d9dd
dmoka <mokabme@gmail.com>
2022-12-20 12:10:14 +0100

    refactoring

--- dca/src/tests/schedule.rs
+++ dca/src/tests/schedule.rs
@@ -23,29 +23,27 @@ use pretty_assertions::assert_eq;
 use sp_runtime::DispatchError;
 use sp_runtime::DispatchError::BadOrigin;
 use sp_runtime::BoundedVec;
+use sp_runtime::traits::ConstU32;
 
 #[test]
 fn schedule_should_store_schedule_for_next_block_when_no_blocknumber_specified() {
     ExtBuilder::default().build().execute_with(|| {
         //Arrange
-        let trades = vec![Trade {
+        let trades = create_bounded_vec(vec![Trade {
             asset_in: 3,
             asset_out: 4,
             pool: PoolType::XYK
-        }];
-
-        let bounded_vec: BoundedVec<Trade, sp_runtime::traits::ConstU32<5>> =
-            trades.try_into().unwrap();
+        }]);
 
         let schedule = Schedule {
-            period: 1,
+            period: 10,
             order: Order {
                 asset_in: 3,
                 asset_out: 4,
                 amount_in: 1000,
                 amount_out: 2000,
                 limit: 0,
-                route: bounded_vec
+                route: trades
             },
             recurrence: Recurrence::Fixed
         };
@@ -57,11 +55,36 @@ fn schedule_should_store_schedule_for_next_block_when_no_blocknumber_specified()
             Option::None
         ));
 
-        let stored_schedule = Dca::schedules(1).unwrap();
-
-
         //Assert
+        let stored_schedule = Dca::schedules(1).unwrap();
+        assert_eq!(stored_schedule,
+                   Schedule {
+                       period: 10,
+                       order: Order {
+                           asset_in: 3,
+                           asset_out: 4,
+                           amount_in: 1000,
+                           amount_out: 2000,
+                           limit: 0,
+                           route: create_bounded_vec(vec![Trade {
+                               asset_in: 3,
+                               asset_out: 4,
+                               pool: PoolType::XYK
+                           }])
+                       },
+                       recurrence: Recurrence::Fixed
+                   })
     });
+}
+
+fn create_bounded_vec(trades: Vec<Trade>) -> BoundedVec<Trade, ConstU32<5>> {
+    let bounded_vec: BoundedVec<Trade, sp_runtime::traits::ConstU32<5>> =
+        trades.try_into().unwrap();
+    bounded_vec
+}
+
+pub fn set_block_number(n: u64) {
+    System::set_block_number(n);
 }
 
 //TODO: add negative case for validating block numbers

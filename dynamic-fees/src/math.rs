abcffbd0a2f546d22ef126bdb6ae3ecde279cb7f
Martin <martin.hloska@gmail.com>
2023-02-08 10:07:37 +0100

    dry fee calcs

--- dynamic-fees/src/math.rs
+++ dynamic-fees/src/math.rs
@@ -1,3 +1,4 @@
+use crate::math::NetVolumeDirection::*;
 use crate::{Balance, Fee};
 use sp_runtime::traits::{Saturating, Zero};
 use sp_runtime::{FixedPointNumber, FixedU128};
@@ -8,6 +9,21 @@ pub struct AssetVolume {
     pub liquidity: Balance,
 }
 
+impl AssetVolume {
+    fn net_volume(&self, direction: NetVolumeDirection) -> (Balance, bool) {
+        match direction {
+            OutIn => (
+                self.amount_out.abs_diff(self.amount_in),
+                self.amount_out < self.amount_in,
+            ),
+            InOut => (
+                self.amount_out.abs_diff(self.amount_in),
+                self.amount_out > self.amount_in,
+            ),
+        }
+    }
+}
+
 pub struct FeeParams {
     pub(crate) max_fee: Fee,
     pub(crate) min_fee: Fee,
@@ -15,54 +31,18 @@ pub struct FeeParams {
     pub(crate) amplification: FixedU128,
 }
 
-pub fn recalculate_asset_fee(volume: AssetVolume, previous_fee: Fee, last_block_diff: u128, params: FeeParams) -> Fee {
-    let decaying = params
-        .decay
-        .saturating_mul(FixedU128::from(last_block_diff.saturating_sub(1)));
-    let fee = FixedU128::from(previous_fee);
-    let s = Fee::from_rational(fee.saturating_sub(decaying).into_inner(), FixedU128::DIV);
-    let previous_fee = s.max(params.min_fee);
-
-    let v_o = volume.amount_out;
-    let v_i = volume.amount_in;
-    let liquidity = volume.liquidity;
-    // x = (V0 - Vi) / L
-    let (x, x_neg) = if liquidity != Balance::zero() {
-        (FixedU128::from_rational(v_o.abs_diff(v_i), liquidity), v_o < v_i)
-    } else {
-        (FixedU128::zero(), false)
-    };
-
-    let a_x = params.amplification.saturating_mul(x);
-
-    let (delta_f, neg) = if x_neg {
-        (a_x.saturating_add(params.decay), true)
-    } else if a_x > params.decay {
-        (a_x.saturating_sub(params.decay), false)
-    } else {
-        (params.decay.saturating_sub(a_x), true)
-    };
-
-    let left = if neg {
-        FixedU128::from(previous_fee)
-            .saturating_sub(delta_f)
-            .max(FixedU128::from(params.min_fee))
-    } else {
-        FixedU128::from(previous_fee)
-            .saturating_add(delta_f)
-            .max(FixedU128::from(params.min_fee))
-    };
-
-    let f_plus = left.min(FixedU128::from(params.max_fee));
-
-    Fee::from_rational(f_plus.into_inner(), FixedU128::DIV)
+#[derive(PartialEq, Eq, Debug)]
+enum NetVolumeDirection {
+    OutIn,
+    InOut,
 }
 
-pub fn recalculate_protocol_fee(
+fn recalculate_fee(
     volume: AssetVolume,
     previous_fee: Fee,
     last_block_diff: u128,
     params: FeeParams,
+    direction: NetVolumeDirection,
 ) -> Fee {
     let decaying = params
         .decay
@@ -71,12 +51,10 @@ pub fn recalculate_protocol_fee(
     let s = Fee::from_rational(fee.saturating_sub(decaying).into_inner(), FixedU128::DIV);
     let previous_fee = s.max(params.min_fee);
 
-    let v_o = volume.amount_out;
-    let v_i = volume.amount_in;
-    let liquidity = volume.liquidity;
     // x = (V0 - Vi) / L
-    let (x, x_neg) = if liquidity != Balance::zero() {
-        (FixedU128::from_rational(v_o.abs_diff(v_i), liquidity), v_o > v_i)
+    let (x, x_neg) = if volume.liquidity != Balance::zero() {
+        let (diff, neg) = volume.net_volume(direction);
+        (FixedU128::from_rational(diff, volume.liquidity), neg)
     } else {
         (FixedU128::zero(), false)
     };
@@ -104,4 +82,17 @@ pub fn recalculate_protocol_fee(
     let f_plus = left.min(FixedU128::from(params.max_fee));
 
     Fee::from_rational(f_plus.into_inner(), FixedU128::DIV)
+}
+
+pub fn recalculate_asset_fee(volume: AssetVolume, previous_fee: Fee, last_block_diff: u128, params: FeeParams) -> Fee {
+    recalculate_fee(volume, previous_fee, last_block_diff, params, OutIn)
+}
+
+pub fn recalculate_protocol_fee(
+    volume: AssetVolume,
+    previous_fee: Fee,
+    last_block_diff: u128,
+    params: FeeParams,
+) -> Fee {
+    recalculate_fee(volume, previous_fee, last_block_diff, params, InOut)
 }

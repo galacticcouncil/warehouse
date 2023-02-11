f506b5cc94f4ae89b6cdc5fc1e9dc634c9e02818
Valery Gantchev <v@lery.nl>
2023-02-12 00:01:18 +0100

    fix bench tests

--- otc/src/benchmarks.rs
+++ otc/src/benchmarks.rs
@@ -7,66 +7,70 @@ use frame_benchmarking::benchmarks;
 use frame_support::assert_ok;
 use frame_system::RawOrigin;
 use hydradx_traits::Registry;
-
 pub const ONE: Balance = 1_000_000_000_000;
-
-const HDX: u32 = 0;
-const DAI: u32 = 1;
 
 benchmarks! {
     where_clause { where
         T::AssetId: From<u32>,
         T::Currency: MultiCurrencyExtended<T::AccountId, Amount=i128>,
         T: crate::pallet::Config,
+        u32: From<<T as pallet::Config>::AssetId>,
     }
   place_order {
-        prepare::<T>()?;
+        let (hdx, dai) = prepare::<T>()?;
 
-        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1)?;
-  }:  _(RawOrigin::Signed(owner.clone()), DAI.into(), HDX.into(), 20 * ONE, 100 * ONE, true)
+        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1, vec!(hdx, dai))?;
+  }:  _(RawOrigin::Signed(owner.clone()), dai.into(), hdx.into(), 20 * ONE, 100 * ONE, true)
     verify {
         let reserve_id = named_reserve_identifier(0);
-        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, HDX.into(), &owner), 100 * ONE);
+        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, hdx.into(), &owner), 100 * ONE);
     }
 
     fill_order {
-        prepare::<T>()?;
+        let (hdx, dai) = prepare::<T>()?;
 
-        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1)?;
-        let filler: T::AccountId = create_account_with_balances::<T>("filler", 2)?;
+        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1, vec!(hdx, dai))?;
+        let filler: T::AccountId = create_account_with_balances::<T>("filler", 2, vec!(hdx, dai))?;
 
         assert_ok!(
-            crate::Pallet::<T>::place_order(RawOrigin::Signed(owner.clone()).into(), DAI.into(), HDX.into(), 20 * ONE, 100 * ONE, true)
+            crate::Pallet::<T>::place_order(RawOrigin::Signed(owner.clone()).into(), dai.into(), hdx.into(), 20 * ONE, 100 * ONE, true)
         );
-  }:  _(RawOrigin::Signed(filler.clone()), 0u32, DAI.into(), 10 * ONE)
+  }:  _(RawOrigin::Signed(filler.clone()), 0u32, dai.into(), 10 * ONE)
     verify {
         let reserve_id = named_reserve_identifier(0);
-        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, HDX.into(), &owner), 50 * ONE);
+        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, hdx.into(), &owner), 50 * ONE);
     }
 
     cancel_order {
-        prepare::<T>()?;
+        let (hdx, dai) = prepare::<T>()?;
 
-        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1)?;
+        let owner: T::AccountId = create_account_with_balances::<T>("owner", 1, vec!(hdx, dai))?;
         assert_ok!(
-            crate::Pallet::<T>::place_order(RawOrigin::Signed(owner.clone()).into(), DAI.into(), HDX.into(), 20 * ONE, 100 * ONE, true)
+            crate::Pallet::<T>::place_order(RawOrigin::Signed(owner.clone()).into(), dai.into(), hdx.into(), 20 * ONE, 100 * ONE, true)
         );
   }:  _(RawOrigin::Signed(owner.clone()), 0u32)
     verify {
         let reserve_id = named_reserve_identifier(0);
-        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, HDX.into(), &owner), 0);
+        assert_eq!(T::Currency::reserved_balance_named(&reserve_id, hdx.into(), &owner), 0);
     }
 }
 
-fn prepare<T: Config>() -> DispatchResult {
+fn prepare<T: Config>() -> Result<(u32, u32), DispatchError>
+where
+    u32: From<<T as pallet::Config>::AssetId>,
+{
     // Register new asset in asset registry
-    T::AssetRegistry::create_asset(&b"HDX".to_vec(), ONE)?;
-    T::AssetRegistry::create_asset(&b"DAI".to_vec(), ONE)?;
+    let hdx = T::AssetRegistry::create_asset(&b"HDX".to_vec(), ONE)?;
+    let dai = T::AssetRegistry::create_asset(&b"DAI".to_vec(), ONE)?;
 
-    Ok(())
+    Ok((hdx.into(), dai.into()))
 }
 
-fn create_account_with_balances<T: Config>(name: &'static str, index: u32) -> Result<T::AccountId, DispatchError>
+fn create_account_with_balances<T: Config>(
+    name: &'static str,
+    index: u32,
+    assets: Vec<u32>,
+) -> Result<T::AccountId, DispatchError>
 where
     T::AssetId: From<u32>,
     T::Currency: MultiCurrencyExtended<T::AccountId, Amount = i128>,
@@ -75,8 +79,10 @@ where
     let account_id: T::AccountId = account(name, index, index);
 
     let token_amount: Balance = 200 * ONE;
-    T::Currency::update_balance(0.into(), &account_id, token_amount as i128)?;
-    T::Currency::update_balance(1.into(), &account_id, token_amount as i128)?;
+
+    for asset in assets.iter() {
+        T::Currency::update_balance(asset.clone().into(), &account_id, token_amount as i128)?;
+    }
 
     Ok(account_id)
 }

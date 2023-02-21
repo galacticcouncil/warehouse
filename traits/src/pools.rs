740b7bfd1971ace705b272ce1ae57f403ae1fb18
dmoka <mokabme@gmail.com>
2023-02-21 13:17:10 +0100

    add traits interfaces for DCA

--- traits/src/pools.rs
+++ traits/src/pools.rs
@@ -1,3 +1,5 @@
+use frame_support::sp_runtime::DispatchResult;
+
 pub trait SpotPriceProvider<AssetId> {
     type Price;
 
@@ -18,4 +20,32 @@ pub trait DustRemovalAccountWhitelist<AccountId> {
 
     /// Remove an account from the list.
     fn remove_account(account: &AccountId) -> Result<(), Self::Error>;
+}
+
+/// AMM trader to define trading functionalities
+pub trait AMMTrader<Origin, AssetId, Balance> {
+    fn sell(
+        origin: Origin,
+        asset_in: AssetId,
+        asset_out: AssetId,
+        amount: Balance,
+        min_buy_amount: Balance,
+    ) -> DispatchResult;
+
+    fn buy(
+        origin: Origin,
+        asset_in: AssetId,
+        asset_out: AssetId,
+        amount: Balance,
+        max_sell_amount: Balance,
+    ) -> DispatchResult;
+}
+
+pub trait PriceProvider<AssetId> {
+    type Price;
+
+    /// Return spot price for given asset pair
+    ///
+    /// Returns None if such pair does not exist
+    fn spot_price(asset_a: AssetId, asset_b: AssetId) -> Option<Self::Price>;
 }

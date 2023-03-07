936990155f9cb363df9711d5e8521cef8ca3f71f
martinfridrich <mrtnfridrich@gmail.com>
2023-03-07 10:27:15 +0100

    fixed LM update_yield_farm_multiplier() validation

--- liquidity-mining/src/lib.rs
+++ liquidity-mining/src/lib.rs
@@ -702,7 +702,10 @@ impl<T: Config<I>, I: 'static> Pallet<T, I> {
         amm_pool_id: T::AmmPoolId,
         multiplier: FarmMultiplier,
     ) -> Result<YieldFarmId, DispatchError> {
-        ensure!(!multiplier.is_zero(), Error::<T, I>::InvalidMultiplier);
+        ensure!(
+            multiplier.ge(&MIN_YIELD_FARM_MULTIPLIER),
+            Error::<T, I>::InvalidMultiplier
+        );
 
         let yield_farm_id =
             Self::active_yield_farm(amm_pool_id.clone(), global_farm_id).ok_or(Error::<T, I>::YieldFarmNotFound)?;

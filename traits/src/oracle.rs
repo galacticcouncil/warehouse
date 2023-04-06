ffbcd21f16831ffefd5ce086cb788c4c1412e613
dmoka <mokabme@gmail.com>
2023-04-06 10:36:13 +0200

    use associated type for arbitrary price oracle

--- traits/src/oracle.rs
+++ traits/src/oracle.rs
@@ -22,8 +22,10 @@ impl<AssetId, Price> NativePriceOracle<AssetId, Price> for () {
 /// Implementers of this trait provide the price for an arbitrary asset pair.
 ///
 /// Should return `None` if no price is available.
-pub trait PriceOracle<AssetId, Price> {
-    fn price(asset_a: AssetId, asset_b: AssetId, period: OraclePeriod) -> Option<Price>;
+pub trait PriceOracle<AssetId> {
+    type Price;
+
+    fn price(asset_a: AssetId, asset_b: AssetId, period: OraclePeriod) -> Option<Self::Price>;
 }
 
 pub struct AlwaysPriceOfOne;

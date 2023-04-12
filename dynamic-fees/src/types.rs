070a34512258c7dad2e5edd7fd9830a06060c9b6
Martin <martin.hloska@gmail.com>
2023-04-12 10:58:00 +0200

    pub fields in fee params

--- dynamic-fees/src/types.rs
+++ dynamic-fees/src/types.rs
@@ -10,10 +10,10 @@ use scale_info::TypeInfo;
 #[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug, TypeInfo)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct FeeParams<Fee> {
-    pub(crate) min_fee: Fee,
-    pub(crate) max_fee: Fee,
-    pub(crate) decay: FixedU128,
-    pub(crate) amplification: FixedU128,
+    pub min_fee: Fee,
+    pub max_fee: Fee,
+    pub decay: FixedU128,
+    pub amplification: FixedU128,
 }
 
 #[derive(Encode, Decode, Eq, PartialEq, Copy, Clone, RuntimeDebug, TypeInfo, MaxEncodedLen)]

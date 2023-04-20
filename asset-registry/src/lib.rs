cc5f2abd167171ea32ce5eb63203fd37c2fa6943
dmoka <mokabme@gmail.com>
2023-04-20 15:45:37 +0200

    make the rate limit getter public to be used in hydra runtime

--- asset-registry/src/lib.rs
+++ asset-registry/src/lib.rs
@@ -580,7 +580,7 @@ impl<T: Config> GetByKey<T::AssetId, T::Balance> for Pallet<T> {
 }
 
 /// Allows querying the XCM rate limit for an asset by its id.
-struct XcmRateLimitsInRegistry<T>(PhantomData<T>);
+pub struct XcmRateLimitsInRegistry<T>(PhantomData<T>);
 /// Allows querying the XCM rate limit for an asset by its id.
 /// Both a unknown asset and an unset rate limit will return `None`.
 impl<T: Config> GetByKey<T::AssetId, Option<T::Balance>> for XcmRateLimitsInRegistry<T> {
